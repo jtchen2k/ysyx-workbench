30de24dfcb397f0d29b5e745ff11f2fff15193b5
tracer-ysyx <tracer@ysyx.org>
2024-11-15 16:00:40 -0800

    > sim RTL ysyx_100013064 陈俊潼 Linux ubuntu-linux-2404 6.8.0-48-generic #48-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 27 14:35:45 UTC 2024 aarch64 aarch64 aarch64 GNU/Linux  16:00:40 up  1:10,  1 user,  load average: 2.68, 2.39, 2.24

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <assert.h>
+#include "verilated.h"
+#include "Vtop.h"
 
-int main() {
-  printf("Hello, ysyx!\n");
+
+int main(int argc, char** argv) {
+  VerilatedContext *contextp = new VerilatedContext();
+  contextp->commandArgs(argc, argv);
+  Vtop *top = new Vtop(contextp);
+  int n = 10;
+  while (n--) {
+    int a = rand() & 1;
+    int b = rand() & 1;
+    top->a = a;
+    top->b = b;
+    top->eval();
+    printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+    assert(top->f == (a ^ b));
+  }
+  printf("Verification complete.\n");
   return 0;
 }

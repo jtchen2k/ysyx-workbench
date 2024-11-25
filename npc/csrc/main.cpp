67ab8f1ecac6c7b7eed0fa5c6cbb176347e012a4
Juntong Chen <dev@jtchen.io>
2024-11-24 18:34:23 -0800

    NJU-ProjectN/nvboard master initialized without tracing
    NJU-ProjectN/nvboard a48416ef8179faa6e40df1355cd9c613af8382f6 fix compile error with clang in MacOS

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <assert.h>
+#include <memory>
+#include "verilated.h"
+#include "Vtop.h"
 
-int main() {
-  printf("Hello, ysyx!\n");
+
+int main(int argc, char** argv) {
+  const std::unique_ptr<VerilatedContext> contextp {new VerilatedContext()};
+  contextp->commandArgs(argc, argv);
+  const std::unique_ptr<Vtop> top {new Vtop{contextp.get()}};
+  contextp->randReset(43);
+  int n = 10;
+  while (n--) {
+    int a = rand() & 1;
+    int b = rand() & 1;
+    top->a = a;
+    top->b = b;
+    top->eval();
+    printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+    assert(top->f == (a ^ b));
+  }
+  printf("Verification complete.\n");
   return 0;
 }

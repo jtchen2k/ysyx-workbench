197e86e388514025f3c7838df5573c99c467e40c
tracer-ysyx <tracer@ysyx.org>
2024-11-24 23:11:07 -0800

    > sim RTL ysyx_100013064 陈俊潼 Linux billc-flex 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:11:07 up 10:27,  4 users,  load average: 0.12, 0.27, 0.35

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <assert.h>
 #include <memory>
-#include "verilated.h"
+#include <verilated.h>
+#include <verilated_vcd_c.h>
 #include "Vtop.h"
 
 #define MAX_SIM_TIME 20
@@ -9,16 +10,22 @@ vluint64_t sim_time = 0;
 
 int main(int argc, char** argv) {
   VerilatedContext *contextp = new VerilatedContext();
+  Verilated::traceEverOn(true);
   contextp->commandArgs(argc, argv);
+
   Vtop *top = new Vtop(contextp);
   contextp->randReset(43);
-  int n = 10;
+
+  VerilatedVcdC *m_trace = new VerilatedVcdC();
+  m_trace->open("waveform.vcd");
+
   while (sim_time < MAX_SIM_TIME) {
     int a = rand() & 1;
     int b = rand() & 1;
     top->a = a;
     top->b = b;
     top->eval();
+    m_trace->dump(sim_time);
     printf("a = %d, b = %d, f = %d\n", a, b, top->f);
     assert(top->f == (a ^ b));
     sim_time++;

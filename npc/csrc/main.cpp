d497f11121789926e1861dcd8de1e7659f497a81
tracer-ysyx <tracer@ysyx.org>
2024-12-01 17:55:43 -0800

    > sim RTL ysyx_100013064 陈俊潼 Linux billc-flex 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:55:43 up 1 day,  5:32,  4 users,  load average: 1.53, 1.04, 0.72

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,40 +1,75 @@
-#include "Vtop.h"
+
+
+
+#include "Vlight.h"
 #include <assert.h>
-#include <memory>
-#include <stdio.h>
+#include <cstdio>
+#include <cassert>
 #include <verilated.h>
 #include <verilated_vcd_c.h>
+#include <nvboard.h>
 
-#define MAX_SIM_TIME 50
+#ifndef TOP_NAME
+#define TOP_NAME Vlight
+#endif
 
-int main(int argc, char **argv) {
-    VerilatedContext *contextp = new VerilatedContext;
-    contextp->commandArgs(argc, argv);
-    contextp->randReset(43);
-    Verilated::traceEverOn(true);
 
-    Vtop *top = new Vtop(contextp);
+static TOP_NAME dut;
 
-    // Set up waveform tracing:
-    // https://veripool.org/guide/latest/faq.html#how-do-i-generate-waveforms-traces-in-c
-    VerilatedVcdC *m_trace = new VerilatedVcdC();
-    top->trace(m_trace, 5);
-    m_trace->open("waveform.fsd");
+void nvboard_bind_all_pins(TOP_NAME *top);
 
-    while (contextp->time() < MAX_SIM_TIME && !contextp->gotFinish()) {
-        contextp->timeInc(1);
-        int a = rand() & 1;
-        int b = rand() & 1;
-        top->a = a;
-        top->b = b;
-        top->eval();
-        m_trace->dump(contextp->time());
-        printf("a = %d, b = %d, f = %d\n", a, b, top->f);
-        assert(top->f == (a ^ b));
-    }
-    printf("Verification complete.\n");
-
-    m_trace->close();
-    delete top;
-    exit(EXIT_SUCCESS);
+static void single_cycle() {
+    dut.clk = 0; dut.eval();
+    dut.clk = 1; dut.eval();
 }
+
+static void reset(int n) {
+    dut.rst = 1;
+    while (n -- > 0) single_cycle();
+    dut.rst = 0;
+}
+
+int main() {
+    nvboard_bind_all_pins(&dut);
+    nvboard_init();
+
+    reset(10);
+
+    while (1) {
+        nvboard_update();
+        single_cycle();
+    }
+    return 0;
+}
+
+// int main(int argc, char **argv) {
+//     VerilatedContext *contextp = new VerilatedContext;
+//     contextp->commandArgs(argc, argv);
+//     contextp->randReset(43);
+//     Verilated::traceEverOn(false);
+
+//     Vtop *top = new Vlight()
+
+//     // Set up waveform tracing:
+//     // https://veripool.org/guide/latest/faq.html#how-do-i-generate-waveforms-traces-in-c
+//     VerilatedVcdC *m_trace = new VerilatedVcdC();
+//     top->trace(m_trace, 5);
+//     m_trace->open("waveform.fsd");
+
+//     while (contextp->time() < MAX_SIM_TIME && !contextp->gotFinish()) {
+//         contextp->timeInc(1);
+//         int a = rand() & 1;
+//         int b = rand() & 1;
+//         top->a = a;
+//         top->b = b;
+//         top->eval();
+//         m_trace->dump(contextp->time());
+//         printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+//         assert(top->f == (a ^ b));
+//     }
+//     printf("Verification complete.\n");
+
+//     m_trace->close();
+//     delete top;
+//     exit(EXIT_SUCCESS);
+// }

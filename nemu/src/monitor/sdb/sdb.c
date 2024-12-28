a96ab930b31a913969df2c69d9fb49adda533c7b
tracer-ysyx <tracer@ysyx.org>
2024-12-27 17:09:48 -0800

    > compile NEMU ysyx_100013064 陈俊潼 Linux billc-flex 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:09:48 up 3 days,  8:28,  4 users,  load average: 1.10, 0.75, 0.52

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -71,7 +71,7 @@ static int cmd_p(char *args) {
 }
 
 static int cmd_info (char *args) {
-  if (strcmp(args, "r") == 0) {
+  if (strcmp(strtok(args, " "), "r") == 0) {
     isa_reg_display();
   } else if (strcmp(args, "w") == 0) {
 

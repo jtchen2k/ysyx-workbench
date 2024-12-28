17ae3fd0ac9428f99e174a2b3818cbba936d41b3
tracer-ysyx <tracer@ysyx.org>
2024-12-27 19:20:19 -0800

    > compile NEMU ysyx_100013064 陈俊潼 Linux billc-flex 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:20:19 up 3 days, 10:50,  4 users,  load average: 0.21, 0.32, 0.29

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -25,7 +25,7 @@ const char *regs[] = {
 
 void isa_reg_display() {
   for (int i = 0; i < 32; i++) {
-    printf("%-8s 0x%08x %-12d\n", reg_name(i), gpr(i), gpr(i));
+    printf("%-8s 0x%08x %12d\n", reg_name(i), gpr(i), gpr(i));
   }
 }
 

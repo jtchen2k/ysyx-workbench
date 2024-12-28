2b43822b725cd4ebaf1548e6c93c2df76db08364
tracer-ysyx <tracer@ysyx.org>
2024-12-27 16:55:53 -0800

    > compile NEMU ysyx_100013064 陈俊潼 Linux billc-flex 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:55:53 up 3 days,  8:13,  4 users,  load average: 0.43, 0.35, 0.31

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -52,6 +52,16 @@ static int cmd_q(char *args) {
   return -1;
 }
 
+static int cmd_si(char *args) {
+  int64_t n = args != NULL ? atoll(args) : 1;
+  if (n < 0) {
+    printf("Invalid argument: %s\n.", args);
+    return 0;
+  }
+  cpu_exec(n);
+  return 0;
+}
+
 static int cmd_help(char *args);
 
 static struct {
@@ -62,9 +72,8 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
   /* TODO: Add more commands */
-
+  { "si", "Step N instructions. If N is not specified, step 1. Usage: si [N]", cmd_si }
 };
 
 #define NR_CMD ARRLEN(cmd_table)

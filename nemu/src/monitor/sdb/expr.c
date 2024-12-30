3923e35efef58dba2577b9bc98a6393174227a2a
tracer-ysyx <tracer@ysyx.org>
2024-12-29 22:55:47 -0800

    > compile NEMU ysyx_100013064 陈俊潼 Linux billc-flex 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:55:47 up 4 days, 15:16,  4 users,  load average: 0.43, 0.32, 0.28

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -271,6 +271,7 @@ static word_t eval(int p, int q, bool* success) {
       case TK_HEX:
         return MUXDEF(CONFIG_ISA64, strtoll(tokens[p].str + 2, NULL, 16), strtol(tokens[p].str + 2, NULL, 16));
       case TK_REG:
+        printf("evaluating register %s\n", tokens[p].str);
         return isa_reg_str2val(tokens[p].str + 1, success);
       default:
         *success = false;

b238ea295c83d570c5d079f9ca49ab2e8307f85a
tracer-ysyx <tracer@ysyx.org>
2025-01-02 14:30:45 -0800

    > compile NEMU ysyx_100013064 陈俊潼 Linux billc-flex 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:30:45 up 6 days, 14:01,  4 users,  load average: 0.57, 0.32, 0.18

--- nemu/scripts/scripts.mk
+++ nemu/scripts/scripts.mk
@@ -1,4 +1,4 @@
 .PHONY: count
 count:
-	@find . -type f -name "*.[ch]" -not -path "./tools/*" -exec cat {} + | wc -l | awk '{ printf "total number of lines: %d\n", $$0 }'
-	@echo "edits made to the main branch: $(shell git diff master --shortstat)"
+	@find . -type f -name "*.[ch]" -not -path "./tools/*" -exec cat {} + | grep -v "^$$" | wc -l | awk '{ printf "total number of lines: %d\n", $$0 }'
+	@echo "edits made to the main branch:$(shell git diff master --shortstat)"
--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -13,7 +13,6 @@
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 
-#include "utils.h"
 #include <cpu/cpu.h>
 #include <cpu/decode.h>
 #include <cpu/difftest.h>

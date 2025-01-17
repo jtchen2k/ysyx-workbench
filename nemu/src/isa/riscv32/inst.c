4b94578be3d5517799683c4b58a4d06842954b42
tracer-ysyx <tracer@ysyx.org>
2025-01-16 23:03:55 -0800

    > compile NEMU ysyx_100013064 陈俊潼 Linux billc-flex 5.15.167.4-microsoft-standard-WSL2 #1 SMP Tue Nov 5 00:21:55 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:03:55 up 2 days, 11:02,  2 users,  load average: 0.63, 0.49, 0.39

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -32,7 +32,7 @@ enum {
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
-// #define immJ() do { *imm = (SEXT)}
+#define immJ() do { *imm = SEXT(BITS(i, 31, 31) << 20 | BITS(i, 30, 21) << 1 | BITS(i, 19, 12) << 12, 12); } while(0)
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst;
@@ -43,7 +43,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_I: src1R();          immI(); break;
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
-    case TYPE_J:
+    case TYPE_J:                   immJ(); break;
     case TYPE_N: break;
     default: panic("unsupported type = %d", type);
   }
@@ -68,10 +68,10 @@ static int decode_exec(Decode *s) {
   INSTPAT("??????? ????? ????? ??? ????? 01101 11", lui, U, R(rd) = imm);
   INSTPAT("??????? ????? ????? 000 ????? 00100 11", addi, I, R(rd) = src1 + imm);
   INSTPAT("??????? ????? ????? 100 ????? 00100 11", xori, I, R(rd) = Mr(src1, 1) ^ imm);
+  INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal, J, R(rd) = s->pc + 4; s->dnpc = s->pc + imm);
 
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
-  // INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal,    J, R(rd) = Mr(src1, 1) + imm);
   INSTPAT_END();
 
   R(0) = 0; // reset $zero to 0
